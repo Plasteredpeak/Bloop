680e87c89ba7200389611bafe8ea1da8e782130c
Ali Hamza <hamza120025@gmail.com>
2022-08-14 00:04:52 +0500

    added auth and bottom navigation

--- App.js
+++ App.js
@@ -1,58 +1,86 @@
 import 'react-native-gesture-handler';
 import React, {useEffect, useState} from 'react';
-import {Button, Text, View, StyleSheet, TextInput} from 'react-native';
-import {Colors} from './Design/Colors';
 
 import SplashScreen from './screens/SplashScreen';
 import LoginScreen from './screens/LoginScreen';
 import SignUpScreen from './screens/SignUpScreen';
+import TabStack from './screens/TabStack';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import {AuthContext} from './components/context';
+
+import Auth from '@react-native-firebase/auth';
+
 const Stack = createStackNavigator();
 
 const App = ({route, navigation}) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState(null);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: foundUser => {
+        setUserToken(foundUser);
+        //setIsLoading(false);
+        console.log('user token: ', userToken);
+      },
+      signOut: () => {
+        setUserToken(null);
+        //setIsLoading(false);
+        console.log('User SignedOut');
+      },
+      signUp: CreatedUser => {
+        setUserToken(CreatedUser);
+        //setIsLoading(false);
+        //console.log('user token: ', userToken.uid);
+      },
+    }),
+    [],
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      Auth().onAuthStateChanged(user => {
+        if (user) {
+          setUserToken(user.uid);
+          console.log('Logged In User:', user.email);
+        }
+      });
+      setIsLoading(false);
+    }, 1500);
+  }, []);
+
+  if (isLoading) {
+    return <SplashScreen />;
+  }
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
-          navigation={navigation}
-          route={route}
-        />
-        <Stack.Screen
-          name="SignUpScreen"
-          component={SignUpScreen}
-          navigation={navigation}
-          route={route}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        {userToken !== null ? (
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="TabStack" component={TabStack} />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen
+              name="LoginScreen"
+              component={LoginScreen}
+              navigation={navigation}
+              route={route}
+            />
+            <Stack.Screen
+              name="SignUpScreen"
+              component={SignUpScreen}
+              navigation={navigation}
+              route={route}
+            />
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: 40,
-    padding: 8,
-  },
-  inter: {
-    margin: 24,
-    fontSize: 18,
-    textAlign: 'center',
-    fontFamily: 'Inter',
-    color: Colors.Accent1,
-  },
-  montserrat: {
-    margin: 24,
-    fontSize: 18,
-    textAlign: 'center',
-    fontFamily: 'Montserrat',
-    color: Colors.Secondary1,
-  },
-});
 
 export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+import {AuthContext} from '../components/context';
+import Auth from '@react-native-firebase/auth';
+
+const HomeScreen = props => {
+  const {signOut} = React.useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+      <Text
+        onPress={() => {
+          Auth().signOut();
+          signOut();
+        }}>
+        Home Screen
+      </Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 40,
+    padding: 8,
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
