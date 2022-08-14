43b151c99755feaa57e986bdf98a826391983c8b
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-08-15 01:00:29 +0500

    git ignored fixed

--- components/Social.js
+++ components/Social.js
@@ -3,60 +3,9 @@ import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 import {Colors} from '../Design/Colors';
 
-import Instagram from '../assets/svgs/instagram';
-import Tiktok from '../assets/svgs/tiktok';
-import Facebook from '../assets/svgs/facebook';
-import Snapchat from '../assets/svgs/snapchat';
-import Twitter from '../assets/svgs/twitter';
-import Linkedin from '../assets/svgs/linkedin';
-import Youtube from '../assets/svgs/youtube';
-import Whatsapp from '../assets/svgs/whatsapp';
-import Telegram from '../assets/svgs/telegram';
-import Viber from '../assets/svgs/viber';
-import Skype from '../assets/svgs/skype';
-import Messenger from '../assets/svgs/messenger';
-import Spotify from '../assets/svgs/spotify';
-import Soundcloud from '../assets/svgs/soundcloud';
-import Twitch from '../assets/svgs/twitch';
-import Tumblr from '../assets/svgs/tumblr';
-import Venmo from '../assets/svgs/venmo';
-import Cashapp from '../assets/svgs/cashapp';
-import Paypal from '../assets/svgs/paypal';
-import Dropbox from '../assets/svgs/dropbox';
-
-import Link from '../assets/svgs/link';
-import Website from '../assets/svgs/website';
+import SocialIcon from '../components/SocialIcon';
 
 const Social = props => {
-  const checkSocial = item => {
-    return (
-      <View>
-        {item == 'Instagram' && <Instagram></Instagram>}
-        {item == 'Tiktok' && <Tiktok></Tiktok>}
-        {item == 'Facebook' && <Facebook></Facebook>}
-        {item == 'Snapchat' && <Snapchat></Snapchat>}
-        {item == 'Twitter' && <Twitter></Twitter>}
-        {item == 'Linkedin' && <Linkedin></Linkedin>}
-        {item == 'Youtube' && <Youtube></Youtube>}
-        {item == 'Whatsapp' && <Whatsapp></Whatsapp>}
-        {item == 'Telegram' && <Telegram></Telegram>}
-        {item == 'Viber' && <Viber></Viber>}
-        {item == 'Skype' && <Skype></Skype>}
-        {item == 'Messenger' && <Messenger></Messenger>}
-        {item == 'Spotify' && <Spotify></Spotify>}
-        {item == 'Soundcloud' && <Soundcloud></Soundcloud>}
-        {item == 'Twitch' && <Twitch></Twitch>}
-        {item == 'Tumblr' && <Tumblr></Tumblr>}
-        {item == 'Venmo' && <Venmo></Venmo>}
-        {item == 'Cashapp' && <Cashapp></Cashapp>}
-        {item == 'Paypal' && <Paypal></Paypal>}
-        {item == 'Dropbox' && <Dropbox></Dropbox>}
-        {item == 'Link' && <Link></Link>}
-        {item == 'Website' && <Website></Website>}
-      </View>
-    );
-  };
-
   return (
     <View style={[styles.flex, {width: props.style}]}>
       {props.socialArray.map(item => {
@@ -69,10 +18,10 @@ const Social = props => {
               props.setSocialVisible(true);
               props.setModal(false);
               props.setSocialSvg(() => {
-                return checkSocial(item);
+                return <SocialIcon item={item}></SocialIcon>;
               });
             }}>
-            {checkSocial(item)}
+            <SocialIcon item={item}></SocialIcon>
             <Text style={styles.text}>{item}</Text>
           </TouchableOpacity>
         );
