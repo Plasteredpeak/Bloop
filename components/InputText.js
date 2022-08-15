7b05ef338cc5a4017a24221c6f21d98fbab93c15
Ali Hamza <hamza120025@gmail.com>
2022-08-15 08:02:08 +0500

    ContactScreen Design Completed

--- components/InputText.js
+++ components/InputText.js
@@ -5,12 +5,20 @@ import {Colors} from '../Design/Colors';
 import User from '../assets/svgs/userIcon.svg';
 import Lock from '../assets/svgs/lockIcon.svg';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 const InputText = ({labelValue, placeholderText, iconType, ...rest}) => {
   return (
     <View style={styles.inputContainer}>
-      <View style={styles.iconStyle}>
-        {iconType === 'user' ? <User /> : <Lock />}
-      </View>
+      {iconType === 'search' ? (
+        <View style={styles.iconStyle2}>
+          <Icon name="ios-search" size={30} color={Colors.Monochrome500} />
+        </View>
+      ) : (
+        <View style={styles.iconStyle}>
+          {iconType === 'user' ? <User /> : <Lock />}
+        </View>
+      )}
       <TextInput
         value={labelValue}
         style={styles.input}
@@ -40,12 +48,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: 50,
   },
+  iconStyle2: {
+    padding: 10,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+  },
   input: {
     padding: 10,
     flex: 1,
     color: 'black',
     justifyContent: 'center',
     alignItems: 'center',
+    fontSize: 16,
   },
 });
 export default InputText;
--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,38 +1,175 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  FlatList,
+  Image,
+} from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
+
+import InputText from '../components/InputText';
 
 import {Colors} from '../Design/Colors';
 
 const ContactScreen = props => {
+  const [search, setSearch] = useState('');
   return (
     <View style={styles.container}>
-      <View style={styles.back}>
-        <Icon
-          name="arrow-back"
-          size={30}
-          color={Colors.Primary1}
-          onPress={() => {
-            props.navigation.goBack();
-          }}
+      <View style={styles.header}>
+        <View style={styles.back}>
+          <Icon
+            name="person-add"
+            size={30}
+            color={Colors.Monochrome100}
+            onPress={() => {}}
+          />
+        </View>
+
+        <View style={styles.title}>
+          <Text style={styles.text}>Contacts</Text>
+          <InputText
+            labelValue={search}
+            onChangeText={search => setSearch(search)}
+            placeholderText={'Name or Username'}
+            iconType={'search'}></InputText>
+        </View>
+      </View>
+      <View style={styles.body}>
+        <FlatList
+          data={[
+            {
+              key: '1',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'John Doe',
+              username: 'johndoe',
+            },
+            {
+              key: '2',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'Jane Doe',
+              username: 'janedoe',
+            },
+            {
+              key: '3',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'John Doe',
+              username: 'johndoe',
+            },
+            {
+              key: '4',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'Jane Doe',
+              username: 'janedoe',
+            },
+            {
+              key: '5',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'John Doe',
+              username: 'johndoe',
+            },
+            {
+              key: '6',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'Jane Doe',
+              username: 'janedoe',
+            },
+            {
+              key: '7',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'John Doe',
+              username: 'johndoe',
+            },
+            {
+              key: '8',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'Jane Doe',
+              username: 'janedoe',
+            },
+            {
+              key: '9',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'John Doe',
+              username: 'johndoe',
+            },
+            {
+              key: '10',
+              img: require('../assets/svgs/defaultuser.png'),
+              name: 'Jane Doe',
+              username: 'janedoe',
+            },
+          ]}
+          keyExtractor={item => item.key}
+          renderItem={({item}) => (
+            <View style={styles.item}>
+              <View style={styles.img}>
+                <Image source={item.img} style={styles.img} />
+              </View>
+              <View style={styles.textContainer}>
+                <Text style={styles.textname}>{item.name}</Text>
+                <Text style={styles.textuser}>@{item.username}</Text>
+              </View>
+            </View>
+          )}
         />
       </View>
-      <Text style={styles.text}>Contact Screen</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: 40,
-    padding: 8,
-    alignItems: 'center',
     backgroundColor: '#FFFFFF',
+    flex: 1,
   },
   back: {
-    margin: 10,
+    alignSelf: 'flex-end',
+    marginTop: 15,
+    marginRight: 15,
+  },
+  title: {
+    alignItems: 'center',
+    paddingBottom: 10,
+  },
+  header: {
+    backgroundColor: Colors.Primary1,
+  },
+  text: {
+    color: Colors.Monochrome100,
+    fontSize: 20,
+    fontFamily: 'Montserrat-SemiBold',
+    fontWeight: 'bold',
+  },
+  body: {
+    flex: 1,
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 15,
+  },
+  img: {
+    width: 75,
+    height: 75,
+    borderRadius: 50,
+    marginHorizontal: 10,
+  },
+  textContainer: {
+    marginHorizontal: 10,
+  },
+  textname: {
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    color: '#242A37',
+  },
+  textuser: {
+    fontSize: 15,
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    color: Colors.Accent1,
   },
 });
 
